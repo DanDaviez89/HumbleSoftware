6e1c0022c9036d3e838887a3891643d9b915a26c
dandaviez89 <dandavies89@yahoo.co.uk>
2022-07-03 19:58:00 +0100

    Creating the User Iterface

---
+++ java.js
@@ -0,0 +1,17 @@
+const table1 = document.querySelector("#table1");
+const table2 = document.querySelector("#table2");
+const table3 = document.querySelector("#table3");
+const table4 = document.querySelector("#table4");
+const table5 = document.querySelector("#table5");
+const table6 = document.querySelector("#table6");
+const table7 = document.querySelector("#table7");
+const table8 = document.querySelector("#table8");
+const table9 = document.querySelector("#table9");
+const table10 = document.querySelector("#table10");
+const table11 = document.querySelector("#table11");
+const table12 = document.querySelector("#table12");
+
+localStorage.setItem('name', 'Bob');
+localStorage.removeItem('name');
+
+sessionStorage.setItem('name', 'John');
