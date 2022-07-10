c5dfaf6efcccb97edcd354b61440b476f42bfc8d
dandaviez89 <dandavies89@yahoo.co.uk>
2022-07-10 18:16:00 +0100

    Adding discount funcion

--- tableSelectJava.js
+++ tableSelectJava.js
@@ -4,13 +4,107 @@ title.textContent = localStorage.getItem("tableID");
 DisplayPrice();
 DisplayOrder();
 
-//Opens up the menu system
-document.getElementById('Add').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "flex";
+//This function gets the total price 
+function GetPrice() 
+{
+    //Fetching the current table
+    var currentTable = localStorage.getItem('tableID');
+    //fetching local storage
+    var tempHolder = localStorage.getItem(currentTable);
+
+    var price = 0;
+
+    if (tempHolder != ""){
+        //Parsing Local Storage
+        var parsed = JSON.parse(tempHolder);
+
+        //Works out price
+        var arrayLength = parsed.length;
+
+        for (var i = 0; i < arrayLength; i++) 
+        {
+            var tempPrice = parsed[i].price;
+            price = price + tempPrice;
+        } 
+    }
+
+    return price;
+}
+
+//Discount Section
+//Opens up the Discount pagge
+document.getElementById('Discount').addEventListener("click", function() {
+	document.querySelector('#discountPage').style.display = "flex";
 });
 
-document.querySelector('.close').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "none";
+document.querySelector('#discountClose').addEventListener("click", function() {
+	document.querySelector('#discountPage').style.display = "none";
+});
+
+//Save over all the menu buttons
+const zero = document.querySelector("#zero");
+const ten = document.querySelector("#ten");
+const twenty = document.querySelector("#twenty");
+const tfive = document.querySelector('#tfive');
+const fifty = document.querySelector('#fifty');
+
+//Discount buttons
+zero.addEventListener("click", function() {
+    var price = GetPrice();
+    price = price;
+
+    //TotalPrice
+    const priceSelector = document.querySelector("#Total");
+    priceSelector.textContent = "£" + price;
+});
+
+ten.addEventListener("click", function() {
+    var price = GetPrice();
+    var discountPrice = price / 100 * 10;
+    var price = price - discountPrice;
+
+    //TotalPrice
+    const priceSelector = document.querySelector("#Total");
+    priceSelector.textContent = "Discount Applied: £" + price;
+});
+
+twenty.addEventListener("click", function() {
+    var price = GetPrice();
+    var discountPrice = price / 100 * 20;
+    var price = price - discountPrice;
+
+    //TotalPrice
+    const priceSelector = document.querySelector("#Total");
+    priceSelector.textContent = "Discount Applied: £" + price;
+});
+
+tfive.addEventListener("click", function() {
+    var price = GetPrice();
+    var discountPrice = price / 100 * 25;
+    var price = price - discountPrice;
+
+    //TotalPrice
+    const priceSelector = document.querySelector("#Total");
+    priceSelector.textContent = "Discount Applied: £" + price;
+});
+
+fifty.addEventListener("click", function() {
+    var price = GetPrice();
+    price = price / 2;
+
+    //TotalPrice
+    const priceSelector = document.querySelector("#Total");
+    priceSelector.textContent = "Discount Applied: £" + price;
+});
+
+//Menu Section
+//Opens up the Menu system
+document.getElementById('Add').addEventListener("click", function() {
+	document.querySelector('#menuSystem').style.display = "flex";
+});
+
+document.querySelector('#menuClose').addEventListener("click", function() {
+	document.querySelector('#menuSystem').style.display = "none";
 });
 
 //Save over all the menu buttons
@@ -48,26 +142,7 @@ function AddToStorage(menuItemName, price) {
 
 function DisplayPrice() 
 {
-    //Fetching the current table
-    var currentTable = localStorage.getItem('tableID');
-    //fetching local storage
-    var tempHolder = localStorage.getItem(currentTable);
-
-    var price = 0;
-
-    if (tempHolder != ""){
-        //Parsing Local Storage
-        var parsed = JSON.parse(tempHolder);
-
-        //Works out price
-        var arrayLength = parsed.length;
-
-        for (var i = 0; i < arrayLength; i++) 
-        {
-            var tempPrice = parsed[i].price;
-            price = price + tempPrice;
-        } 
-    }
+    var price = GetPrice();
 
     //TotalPrice
     const priceSelector = document.querySelector("#Total");
@@ -79,18 +154,21 @@ function DisplayPrice()
 burgers.addEventListener("click", () => {
     AddToStorage("Burger",10);
     DisplayPrice();
+    DisplayOrder();
 });
 
 Breakfasts.addEventListener("click", () => {
     AddToStorage("Breakfasts",11);
     DisplayPrice();
+    DisplayOrder();
 });
 
 
 //Allows to view the storage
 function DisplayOrder()  {
-    //Fetch the orderlist element
+    //Fetch the orderlist element and reset content
     const orderList = document.querySelector("#lsContent");
+    orderList.textContent = "";
 
     //Fetch the table number
     var currentTable = localStorage.getItem('tableID');
@@ -107,7 +185,7 @@ function DisplayOrder()  {
     
         for (var i = 0; i < arrayLength; i++) 
         {
-            orderList.textContent = orderList.textContent + " " + myJSONparsed[i].itemName;
+            orderList.innerHTML = orderList.textContent + " " + myJSONparsed[i].itemName + ": £" + myJSONparsed[i].price;
         } 
     }
 }
